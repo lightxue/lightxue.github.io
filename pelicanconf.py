224169389276ee5dc46ceef9df0b30ac3ece4ba6
lightxue <bkmgtp@gmail.com>
2018-04-09 02:39:46 +0800

    首页增加description
    Signed-off-by: lightxue <bkmgtp@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,6 +6,7 @@ AUTHOR = 'Light Xue'
 SITENAME = '0xFEE1C001'
 SITESUBTITLE = 'Where there is a shell, there is a way'
 SITEURL = 'http://www.lightxue.com'
+DESCRIPTION = '这是个兴趣使然的技术博客。0xFEE1C001是feel cool的hexspeak。主题包括但不限于操作系统、编程语言、数据库、算法、工具。心血来潮才会动手写一篇。渴望能和更多的人交流，欢迎留言、邮件分享你的见解。'
 
 PATH = 'content'
 STATIC_PATHS = [
